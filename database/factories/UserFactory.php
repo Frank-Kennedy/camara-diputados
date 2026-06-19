c1a74e8fabed903eb631b3d99d4513c2c932ca99
unknown <CCEI ADMIN@SWAPPODMGR.cceige.local>
2026-06-19 10:38:55 +0100

    Modificacion de Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
- * @extends Factory<User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
@@ -26,6 +26,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
